42ccf719c4cf259e228d509ad71032419a680ba1
Prashantprasad31 <149814220+Prashantprasad31@users.noreply.github.com>
2023-11-17 19:44:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Full-Stack Developer", "Web Developer", "Data Analyst"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 50,
+    loop: true
+});
